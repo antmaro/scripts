fe6c711845278a27fc7d1b804568d09eec89b9f1
Antonio Marí Romero <anmaro@gmail.com>
2017-06-09 22:02:56 +0200

    Creation of script to obtain the IP address of libvirt domain

---
+++ getlibvirt_IP.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Script that returns the IP Address of a libvirt domain
+
+OPTS=$@
+domain=$1
+
+show_help() {
+        echo "Usage: $0 domain_name"
+        echo 
+}
+
+
+##########
+# main
+#########
+
+if [ ! ${OPTS} ]; then
+        show_help
+        echo $#
+        exit 0
+fi
+
+# Obtain the IP from a domain
+sudo virsh domifaddr ${domain} | grep ipv4 | awk '{print $4}' | cut -d"/" -f1
